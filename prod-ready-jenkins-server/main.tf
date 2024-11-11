35ca05f815bf66feae8eaee39c9922ed7390c952
aiya160701 <aiya160701@gmail.com>
2024-11-11 16:14:38 -0600

    Added github webhook ipv4, ipv6 addresses

--- prod-ready-jenkins-server/main.tf
+++ prod-ready-jenkins-server/main.tf
@@ -40,10 +40,10 @@ resource "aws_route_table" "pub_route_table" {
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    Name = "public-rt"
+    Name = "jenkins-server-public-rt"
   }
   tags_all = {
-    Name = "public-rt"
+    Name = "jenkins-server-public-rt"
   }
 }
 
@@ -51,10 +51,10 @@ resource "aws_route_table" "pub_route_table" {
 resource "aws_route_table" "priv_route_table" {
   vpc_id = aws_vpc.server.id
   tags = {
-    Name = "private-rt"
+    Name = "jenkins-server-private-rt"
   }
   tags_all = {
-    Name = "private-rt"
+    Name = "jenkins-server-private-rt"
   }
 }
 
@@ -71,21 +71,55 @@ resource "aws_route_table_association" "priv_rta" {
   route_table_id = aws_route_table.priv_route_table.id
 }
 
-
-
 #SG JENKINS
 resource "aws_security_group" "jenkins_prod_sg" {
   vpc_id = aws_vpc.server.id
   lifecycle {
     ignore_changes = [description]
   }
+  ingress = [
+    {
+      cidr_blocks = [
+        var.aKumo_ip_address
+      ]
+      description      = "SSH from aKumo office"
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks = [
+        var.aKumo_ip_address,
+            "192.30.252.0/22",
+            "185.199.108.0/22",
+            "140.82.112.0/20",
+            "143.55.64.0/20"
+      ]
+      description = "Port 443 is open for aKumos office and GitHub WebHooks"
+      from_port   = 443
+      ipv6_cidr_blocks = [
+            "2a0a:a440::/29",
+            "2606:50c0::/32"
+      ]
+      
+      prefix_list_ids = []
+      protocol        = "tcp"
+      security_groups = []
+      self            = false
+      to_port         = 443
+    },
+  ]
 }
 
 
 # EC2 Instance WITH CUSTOM AMI
 resource "aws_instance" "jenkins_instance" {
-  ami                         = "ami-0699aedaa3d0ea70f"
-  instance_type               = "t2.medium"
+  ami                         = var.jenkins_server_custom_ami
+  instance_type               = var.instance_type
   key_name                    = "aiya@mac"
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public_subnet.id
@@ -116,7 +150,7 @@ resource "aws_route53_record" "dns_record" {
   zone_id = aws_route53_zone.dns_zone.zone_id
   name    = "jenkins.aiyalida.com"
   type    = "A"
-  ttl     = "15"
+  ttl     = "300"
   records = [aws_instance.jenkins_instance.public_ip]
 }
 
---
+++ prod-ready-jenkins-server/variables.tf
@@ -0,0 +1,32 @@
+variable "github_web_ipv4_addresses" {
+  description = "Github Webhook IPv4 addresses"
+  type = set(string)
+  default = [
+    "192.30.252.0/22",
+    "185.199.108.0/22",
+    "140.82.112.0/20",
+    "143.55.64.0/20"
+  ]
+}
+
+variable "github_web_ipv6_addresses" {
+  description = "Github Webhook IPv6 addresses"
+  type = set(string)
+  default = [
+    "2a0a:a440::/29",
+    "2606:50c0::/32"
+  ]
+}
+
+variable "aKumo_ip_address" {
+  default = "73.74.183.123/32"
+}
+
+variable "instance_type" {
+  default = "t2.medium"
+}
+
+variable "jenkins_server_custom_ami" {
+  description = "This is pre-configured ami for Jenkins Server with NGINX Proxy "
+  default     = "ami-0699aedaa3d0ea70f"
+}
